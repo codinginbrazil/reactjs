81ff722e9b2372e21d753520430df4be45c0b3c2
sswellington <wellws@pm.me>
2021-07-24 15:53:28 -0300

    next.js: css

---
+++ src/pages/_app.js
@@ -0,0 +1,9 @@
+import '../css/new.css'
+import '../css/sarada.css'
+
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+    return < Component {...pageProps} />
+}
+  
